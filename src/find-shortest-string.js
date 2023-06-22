c6e764425bedd3611ec84962cdc7a58376e62ab1
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-22 15:04:47 +0100

    all passed test checks

---
+++ src/find-shortest-string.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Find the shortest string in an array.
+ * In the case of a tie, returns the earlier string in the array.
+ *
+ * @param stringArr array of strings
+ * @returns the shortest string
+ */
+function findShortestString(stringArr) {
+    let shortestString = stringArr[0];
+    for (const str of stringArr) {
+        if (str.length < shortestString.length) {
+            shortestString = str;
+        }
+    }
+    return shortestString;
+}
+exports.default = findShortestString;
